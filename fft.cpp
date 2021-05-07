0ca9ae3c2b8b4eadc2ee087b6c2147d42b54627b
andreasfloros <77194848+andreasfloros@users.noreply.github.com>
2021-05-07 12:56:26 +0300

    Update fft.cpp

--- fft.cpp
+++ fft.cpp
@@ -5,7 +5,6 @@
 // some definitions to make our lives easier
 typedef std :: complex<float> complex_float;
 const float PI = 2 * asin(1);
-const std :: complex<float> I = complex_float(0.0, 1.0);
 
 // overload multiplication since this isn't provided
 complex_float operator * (complex_float a, float b){
@@ -16,34 +15,44 @@ complex_float operator * (float a, complex_float b){
     return b * a;
 }
 
+unsigned int bit_reverse(unsigned int x, int log2x)
+{
+    int y = 0;
+    for (int n = 0; n < log2x; n++)
+    {
+        y <<= 1;
+        y |= (x & 1);
+        x >>= 1;
+    }
+    return y;
+}
+
 // fft will return a pointer to the DFT of the time_series array
 // note that this is allocated on the heap (need to delete manually)
 // time_series array must be of type complex_float
 // see main for example use
 complex_float * fft(complex_float * time_series, int length){
+    const complex_float I = complex_float(0, 1);
     complex_float * ans = new complex_float [length];
-    if (length == 1){
-        ans[0] = time_series[0];
+    int log2x = log2(length);
+    for (unsigned int n = 0; n < length; n++){
+        int rev_idx = bit_reverse(n, log2x);
+        ans[n] = time_series[rev_idx]; 
     }
-    else{
-        int half_length = length / 2;
-        std :: complex<float> even_series [half_length];
-        std :: complex<float> odd_series [half_length];
-        for (int n = 0; n < length; n++){
-            if(n % 2 == 0){
-                even_series[n] = time_series[n];
+    for (int n = 1; n <= log2x; n++){
+        int m = 1 << n;
+        int m2 = m >> 1;
+        complex_float w = complex_float(1, 0);
+        complex_float wm = exp(I * (PI / m2));
+        for (int j = 0; j < m2; j++){
+            for (int k = j; k < length; k += m){
+                complex_float t = w * ans[k + m2];
+                complex_float u = ans[k];
+                ans[k] = u + t;
+                ans[k + m2] = u - t;
             }
-            else{
-                odd_series[n] = time_series[n];
-            }
+            w *= wm;
         }
-        std :: complex<float> * even_ans = fft(even_series, half_length);
-        std :: complex<float> * odd_ans = fft(odd_series, half_length);
-        for (int n = 0; n < length; n++){
-            ans[n] = even_ans[n % half_length] + exp(float(-2) * PI * I * (n / length)) * odd_ans[n % half_length];
-        }
-        delete [] even_ans;
-        delete [] odd_ans;
     }
     return ans;
 }
@@ -59,7 +68,7 @@ void print_complex_array(complex_float * array, int length){
 
 // example call to fft
 int main(){
-    int length = 30;
+    int length = 32;
     float time_series [length] = {1}; // kronecker delta
     complex_float complex_time_series [length];
     for (int n = 0 ; n < length; n++){
